98400f08c110fc90f4e093aee833412948572273
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-08-05 02:51:42 +0700

    Create lesson1_data_types.py

---
+++ lesson1_data_types.py
@@ -0,0 +1,181 @@
+
+"""
+1. Mindset
+2. Limit
+3. Behaviour
+4. Ethics
+5. Imaginary
+6. Step
+7. Simple
+8. Imperative paradigm(procedure, object), Declarative paradigm (functional)
+9. Procedural programming (procedure)
+
+################################################
+
+1. time
+2. select
+3. loop
+4. function
+
+################################################
+
+1. data types
+2. control flow
+3. function
+
+################################################
+
+1. time
+   - top to b
+   - left to right
+   - parenthesis 
+
+################################################
+
+p0  computer open
+p1  song open
+p2  song close
+p3  computer close
+
+################################################
+
+012123
+
+p0 p1
+p2 p1
+p2 p3
+
+p3(p2(p1(p2(p1(p0)))))
+
+################################################
+
+p3(p0) p1(p0)
+p2 p1
+p2 p3
+
+03012123
+
+################################################
+
+abc  ( data types 15 )
+
+apple ( type casting, ... )
+
+paratic
+
+grammer ( syntax )
+
+################################################
+
+1. Data types
+  - str
+    - character (chr)
+    - character string ( str )
+    - documentation string ( doc )
+
+  - number
+    - int       --->   integer 
+    - float     --->   decimal/floating-point number
+    - complex   --->   complex number
+
+  - 8
+    - normal list  2   (list, tuple)
+    - unique list  2   (set, frozenset)
+    - binary list  2   (bytearray, bytes)
+    - number list  1   (range)(generator object)
+    - item list    1   (dict)
+
+  - condition           bool
+    
+  - memoryview
+
+  - None Type           None
+
+################################################
+
+range
+
+1, 2, 3, 4, ..., 100
+start = 1
+stop = 101
+step = 1
+
+################################################
+
+odd 50  = 1, 3, 5, to 99
+start = 1
+stop = 100
+step = 2
+
+even 50 = 2, 4, 6, to 100
+start = 2
+stop = 101
+step = 2
+
+reversed odd 50 = 99, 97, 95 to 1
+start = 99
+stop = 0
+step = -2
+
+reversed even 50 = 100, 98, 96, 94 to 2
+start = 100
+stop = 1
+step = -2
+
+################################################
+
+1. data type
+
+################################################
+
+1.1. built-in data types ( 15, 19 )
+
+1. str
+2. int
+3. float
+4. complex
+5. list
+6. tuple
+7. range
+8. dict
+9. set
+10. frozenset
+11. bool
+12. bytes
+13. bytearray
+14. memoryview
+15. NoneType(None)
+
+################################################
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
