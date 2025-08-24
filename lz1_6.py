478458e6a3665dc30356477cb1b839d4d367795a
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-08-25 01:51:44 +0700

    Create lz1_6.py

---
+++ lz1_6.py
@@ -0,0 +1,180 @@
+"""
+
+1. Sequence  
+2. Usages of data types 
+3. Names of data types
+4. Symbols of data types 
+5. Checking data types 
+6. Boolean values of empty and something 
+7. Mutable, Immutable, Ordered, Unordered 
+7. Indexing 
+8. Slicing 
+10. Operation, Operator, Operand (43)
+11. Unary, Binary, Ternary operator 
+12. Precedence (15)
+13. Associativity (2)
+14. Built-in Operators ( 7 )
+15. Arithmetic operators in programming (35)
+16. Normal statement, Conditional statement (if, elif, else)
+17. Program flow, Control flow, Program quality
+18. naming rules
+
+################################################################################################
+
+1. Sequence 
+   - top, left, parenthesis
+   
+################################################
+    
+2. Usages of data types 
+
+_ Data types
+  - str
+    - character (chr)
+    - character string ( str )
+    - documentation string ( doc )
+
+  - number
+    - int       --->   integer
+    - float     --->   decimal/floating-point number
+    - complex   --->   complex number
+
+  - 8
+    - normal list  2   (list, tuple)
+    - unique list  2   (set, frozenset)
+    - binary list  2   (bytearray, bytes)
+    - number list  1   (range)(generator object)
+    - item list    1   (dict)
+
+  - condition           bool
+
+  - memoryview
+
+  - None Type           None
+
+################################################
+
+3. Names of data types
+
+1. str
+2. int
+3. float
+4. complex
+5. list
+6. tuple
+7. range
+8. dict
+9. set
+10. frozenset
+11. bool
+12. bytes
+13. bytearray
+14. memoryview
+15. NoneType      <---   only notation value None
+
+################################################
+
+4. Symbols of data types
+
+notations
+1. quotes
+2. .
+3. j
+4. []
+5. (), (comma)
+6. {}  (set)
+7. b
+8. {} (dict)
+
+Notation is value.
+1. 1
+2. True, False
+3. None
+
+No notation
+1. frozenset()
+2. bytearray()
+3. range()
+4. memoryview()
+
+################################################
+ 
+5. Checking data types
+
+################################################
+ 
+6. Boolean values of empty and something 
+7. Mutable, Immutable, Ordered, Unordered 
+7. Indexing 
+8. Slicing 
+
+################################################
+
+10. Operation, Operator, Operand (43)
+
+e, 
+u,   -, +, ~
+* /, 
++ -                         
+
+shift                         bitwise
+and       
+xor
+or
+
+C                           
+not                           logical
+and
+or
+
+t                             
+assignment                    =, **=, *=, /=, //=, %=, +=, -=, <<=, >>=, &=, ^=,|=   
+walrus                        :=
+
+################################################
+
+11. Unary, Binary, Ternary operator 
+
+12. Precedence (15)
+
+13. Associativity (2)
+
+################################################
+
+14. Built-in Operators ( 7 )
+
+1. Arithmetic Operators        (9)
+2. Bitwise Operators           (6)
+3. Comparison Operators        (6)
+4. Identity Operators          (2)
+5. Membership Operators        (2)
+6. Logical Operators           (3)
+7. Assignment Operators       (13)
+   Walrus                      (1)
+                            
+   ternary                      1  
+   
+                               43
+                               
+################################################
+
+15. Arithmetic operators in programming (35)
+
+            90350       = 1 day 1:5:50
+          /       |
+        25        350
+      /   |      /  |
+     1    1     5   50
+     
+################################################
+
+16. Normal statement, Conditional statement (if, elif, else)
+
+17. Program flow, Control flow, Program quality
+
+18. naming rules (one, alpha, number, o, r)
+
+################################################################################################
+
+
+"""
