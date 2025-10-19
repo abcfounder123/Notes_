548c1367a06f7eb956082e76ec2f74bd421f695e
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-10-20 00:05:12 +0700

    Create ez_notes_1_6.py

---
+++ ez_notes_1_6.py
@@ -0,0 +1,72 @@
+
+"""
+
+1. Sequence
+2. Usages of data types
+3. Names of data types   
+4. Symbols of data types     1, True, None   
+5. Boolean values of empty and anything
+6. Mutable, Immutable, Ordered, Unordered
+7. range
+8. Indexing
+9. Slicing
+10. Operation, Operator, Operand 
+11. Unary operator, Binary operator, Ternary operator
+12. Precedence (15)(43)           
+13. Associativity (2)
+14. Operator groups( 7 )
+15. Naming rules
+
+################################################
+
+Naming rules
+
+Python has five rules for naming identifiers/ labels. 
+(one, alpha, number, o, r)
+1. An identifier must contain at least one character.          
+2. The first character of an identifier must be an alphabet or the underscore.
+3. The remaining characters may be alphabets, underscores, numbers.
+4. Qther characters are not permitted.
+5. A reserved keyword cannot be used as an identifier.
+6. meaningful
+
+################################################
+
+(one, alpha, number, o, r)
+
+
+one         --->   at least one chr
+alpha       --->   alphabet, underscore
+number      --->   alphabet, underscore, number
+o           --->   other chr
+r           --->   reserved keyword name
+
+################################################
+
+age = 20
+name = "Mg Mg"
+
+_age = 20
+_name = "Mg Mg"
+
+
+1name = "Mg Mg"
+name1 = "Mg Mg"
+
+keyword.py
+
+kwlist = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+35
+
+################################################
+
+import keyword
+
+print(keyword.kwlist)
+
+################################################################################################
+
+"""
+
+
