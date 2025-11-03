bb60aa6b442586d342f0b8c35ac34f57cafb7301
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-11-03 23:38:07 +0700

    notes

---
+++ ez4_day1.py
@@ -0,0 +1,64 @@
+
+"""
+1. Mindset
+2. Limit
+3. Behaviour
+4. Ethics
+5. Imaginary
+6. Steps
+7. Simple    
+8. Imperative paradigm(procedure, object), Declarative paradigm (functional)
+9. Procedural programming (procedure)
+
+################################################
+
+Procedural programming
+1. sequence
+2. selection
+3. looping
+4. function (Procedure)
+
+################################################
+
+1. sequence
+   - top
+   - left
+   - parenthesis first
+
+p1
+p2 p3(p4)
+p5(p6(p7))
+p8(p11) p9 p10(p12)
+
+1 243 765 11 8 9 12 10
+
+p1
+p2 p4(p3)
+p7(p6(p5))
+p9(p8) p10 p12(p11)
+
+################################################
+
+data types 15
+
+1. Character (chr)
+2. Character string (str)
+3. Documentation string (doc)
+
+1. integer (int)
+2. floating-point number (float)
+3. complex number  (complex)
+
+1. normal list (list, tuple)
+2. unique list (set, frozenset)
+3. binary list (bytearray, bytes)
+7. number list (range)
+8. item list   (dict) dictionary
+
+################################################################################################
+
+"""
+
+
+
+
