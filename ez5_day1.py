cd8dd2e3e4dd68a10ae157f90e64f29742acde7b
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-11-22 12:04:01 +0700

    intro

---
+++ ez5_day1.py
@@ -0,0 +1,73 @@
+
+"""
+1. Mindset
+2. Limit
+3. Behaviour
+4. Ethics
+5. Imaginary
+6. Step
+7. Simple
+8. Imperative paradigm(procedure, object), Declarative paradigm (functional)
+9. Procedural programming (procedure)
+
+################################################
+
+Procedural programming
+1. sequence
+2. selection
+3. looping
+4. function (Procedure)
+
+################################################
+
+1. data types
+2. control flow
+3. function
+
+################################################
+
+1. sequence
+   - top
+   - left
+   - parenthesis
+
+################################################
+
+p2(p1) p3
+p4 p6(p5)
+p7 p8 p9
+p10
+
+12345678910
+
+
+p1(p2) p3
+p4 p5(p6)
+p10
+p9 p7 p8
+
+213 465 10 978
+
+p2(p1) p3(p11(p12))
+p4 p6(p5)  p1(p2(p3))
+p7(p13) p8(p14) p9
+p10
+
+1 2 12 11 3 4 5 6 3 2 1 13 7 14 8 9 10
+
+################################################
+
+Data types 15
+
+1. Character              ->   chr()
+2. Character string       ->   str()
+3. Documentation string   ->   doc
+
+1. integer                 ->   int()
+2. floating-point number   ->   float()
+3. complex number          ->   complex()
+
+################################################
+
+"""
+
