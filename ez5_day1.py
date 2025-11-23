8f561c3f8592f18fcb4d18ce5933fc44510baf2c
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-11-23 11:49:31 +0700

    add day.2

--- ez5_day1.py
+++ ez5_day1.py
@@ -59,15 +59,138 @@ p10
 
 Data types 15
 
-1. Character              ->   chr()
-2. Character string       ->   str()
-3. Documentation string   ->   doc
+1. Character               ->   chr()
+2. Character string        ->   str()
+3. Documentation string    ->   doc
 
 1. integer                 ->   int()
 2. floating-point number   ->   float()
 3. complex number          ->   complex()
 
+1. normal list             ->   list(), tuple()
+2. unique list             ->   set(), frozenset()
+3. binary list             ->   bytearray(), bytes()
+4. number                  ->   range()
+5. item list               ->   dict()
+
+1. Boolean  - True, False  ->   bool()
+
 ################################################
+
+Built-in and Custom
+
+1 + 2.2      =>   3.2
+1kg + 2.2lb  =>   2kg, 4.4lb
+1$ + 4000 kyats => 2$, 8000 kyats
+
+################################################
+
+Mutable and immutable
+
+################################################
+
+range(start, stop, start)
+
+1, 2, 3, 4, ..., 99,
+
+start value = 1
+stop value = 100
+step value = 1
+
+1 to 100
+start value = 1
+stop value = 101
+step value = 1
+
+2, 4, 6, 8,  to 100   =>  range(2, 101, 2)
+start value = 2
+stop value = 101
+step value = 2
+
+1, 3, 5, 7, 9 to 99   =>  range(1, 100, 2)
+start value = 1
+stop value = 100
+step value = 2
+
+100, 98, 96 to 2   =>  range(100, 1, -2)
+start value = 100
+stop value = 1
+step value = -2
+
+99, 97, 95 to 1   =>  range(99, 0, -2)
+
+-1 to -100    =>  range(-1, -101, -1)
+start value = -1
+stop value = -101
+step value = -1
+
+-100, -99, to -1     =>  range(-100, 0, 1)
+-2, -4, -6, -8,  to -100   =>   range(-2, -101, -2)
+-1, -3, -5, -7, -9 to -99  =>   range(-1, -100, -2)
+
+-100, -98, -96 to -2
+range(-100, -1, 2)
+
+255, 260, 265 to 500
+range(255, 501, 5)
+
+0, 1, 2 to 10
+range(0, 11, 1)
+range(0, 11)   # step = 1
+range(11)      # start = 0
+
+range(0, 100, 1)  =>  range(100)
+range(100) # stop = 100, start = ? , step = ?
+
+################################################
+
+Collection and element
+
+collection = [1, 2, 3, 4, 5, 6]
+first element = 1
+
+################################################
+
+item list (dictionary)
+
+item = key   +  value
+
+################################################
+
+15. NoneType
+
+cake = 1
+cake = -1
+cake = 0
+
+data = None
+
+################################################
+
+Data types 15
+
+1. str()
+
+2. int()
+3. float()
+4. complex()
+
+5. list()
+6. tuple()
+7. set()
+8. frozenset()
+9. bytearray()
+10. bytes()
+
+11. range()
+12. dict()
+
+13. bool()              True, False
+
+14. memoryview()
+15. NoneType            None
+
+################################################################################################
 
 """
 
