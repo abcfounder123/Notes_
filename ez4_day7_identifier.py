a732bf583304beffd8895d502c322b2007d28fe6
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-11-25 00:26:50 +0700

    naming rules

---
+++ ez4_day7_identifier.py
@@ -0,0 +1,35 @@
+
+"""
+
+Python has five rules for naming identifiers(labels).
+
+(one, alpha, number, o, r)
+
+1. An identifier must contain at least one character.
+2. The first character of an identifier must be an alphabet or the underscore.
+3. The remaining characters may be alphabets, underscores, numbers.
+4. Other characters are not permitted.
+5. A reserved keyword cannot be used as an identifier.
+6. meaningful
+
+################################################
+
+one, alpha, number, o, r
+
+1. one  (at least one character)
+2. alpha (alphabet or underscroll)
+3. number 
+4. o     (other character)
+5. r     (reserved keyword) 35
+
+################################################
+
+keyword.py
+
+kwlist = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+################################################################################################
+
+
+"""
+
