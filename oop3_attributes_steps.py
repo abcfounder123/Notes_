1d7938bc8e35bf1744649df7b9f508d89af1981b
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-11-28 00:52:12 +0700

    attributes steps4

---
+++ oop3_attributes_steps.py
@@ -0,0 +1,562 @@
+
+"""
+
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+- paradigm ဂရိစကားလုံး တွေးပုံတွေးနည်း
+- concept အယူအဆ / အမြင်
+- object  အရာဝတ္ထု
+
+အရာဝတ္ထုဆိုတဲ့ အယူအဆအပေါ်မှာ အခြေခံထားတဲ့ တွေးပုံတွေးနည်း ဖြစ်ပါတယ်။
+
+အရာဝတ္ထုလို့တွေးပြီး program ရေးတာ ဖြစ်ပါတယ်။
+
+##################################################
+
+Attributes exercises
+
+အရာဝတ္ထုတွေမှာ ကိုယ်ပိုင် ပိုင်ဆိုင်မှုတွေ ရှိကြပါတယ်။
+
+ဒီနေ့သင်ခန်းစာက ပိုင်ဆိုင်မှုနဲ့ပတ်သတ်ပြီး စနစ်တကျ တွေးတတ်ဖို့ ဖြစ်ပါတယ်။
+
+အဆင့်ဆယ်ဆင့်ရှိပြီး ပထမနှစ်ဆင့်ကို လေ့ကျင့်ပြီး အသားကျမှသာ ကျန်တာတွေ ဆက်လေ့လာလို့ရပါမယ်။
+
+##################################################
+
+1. Write
+
+ပထမအဆင့်ကတော့ အတွေးတွေကို ချရေးတာ ဖြစ်ပါတယ်။
+
+အများကြီးနဲ့ ရှုတ်ရှုတ်ထွေးထွေး မတွေးပဲ နည်းနည်းလေးနဲ့ ရိုးရိုးလေးပဲ တွေးရပါမယ်။
+
+ရိုးရိုးလေးတွေးပြီး ရေးတတ်စေချင်တာပါ။
+
+##################################################
+
+2. Divide
+
+ဒုတိယအဆင့်ကတော့ ရေးထားတာကို ခွဲထုတ်တာ ဖြစ်ပါတယ်။
+
+ခွဲထုတ်တဲ့အချိန်မှာ အတွေးတွေကို ဖျောက်ထားရမှာဖြစ်ပြီး အသစ်ထပ်ထည့်တာတွေ မလုပ်ရပါဘူး။
+
+ရေးထားတဲ့အတိုင်းဖတ်ပြီး စက်ရုပ်လိုမျိုး ခွဲထုတ်ပေးရပါမယ်။
+
+ခွဲထုတ်ရင်းနဲ့ data ဘယ်နှစ်ခု၊ function ဘယ်နှစ်ခု စသဖြင့် အရေအတွက်အပါအဝင် ပေးထားတဲ့အမည်တွေနဲ့ အလုပ်လုပ်ပုံတွေကိုပါ တစ်ခါတည်း အလွတ်ရအောင် မှတ်ထားဖို့လည်း လိုအပ်ပါသေးတယ်။
+
+မှန်မှန်ကန်ကန် ခွဲထုတ်တတ်ဖို့နဲ့ မှတ်တတ်တဲ့အကျင့် ရှိစေချင်တာပါ။
+
+##################################################
+
+Python ၏ အနှစ်သာရ
+
+1. လှတာ ပိုကောင်းတယ်။
+
+2. ရှင်းလင်းတာ ပိုကောင်းတယ်။
+
+3. အတွေးတွေဟာ ရိုးရှင်းနေရမယ်။
+
+###################################################################################################
+
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+
+#################################################
+
+Attributes exercises
+
+Step.1   --->   Write
+
+1. Laptop တွင် serial_no ပါသည်။ on, off လုပ်နိုင်သည်။
+
+2. Network_Card တွင် speed ပါသည်။ သတ်မှတ် speed ဖြင့် download ဆွဲနိုင်သည်။
+
+3. 1995 တွင် ပေါ်ပေါက်ခဲ့သော DialUp ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 9600bit/s ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+4. 1999 တွင် ပေါ်ပေါက်ခဲ့သော ADSL ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 2000000bit/s (2Mbit/s)  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+5. 2006 တွင် ပေါ်ပေါက်ခဲ့သော Ethernet ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10Mbit/s  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+6. 2014 တွင်  Ethernet_2014 ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+###################################################################################################
+
+Step.2   --->   Divide
+
+1. Laptop တွင် serial_no ပါသည်။ on, off လုပ်နိုင်သည်။
+class   --->   Laptop
+data    --->   serial_no
+method  --->   on(), off()
+
+2. Network_Card တွင် speed ပါသည်။ သတ်မှတ် speed ဖြင့် download ဆွဲနိုင်သည်။
+class   --->   Network_Card 
+data    --->   speed
+method  --->   download()
+
+3. 1995 တွင် ပေါ်ပေါက်ခဲ့သော DialUp ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 9600bit/s ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->   DialUp 
+data    --->   speed = 9600bit/s
+method  --->   download()
+
+4. 1999 တွင် ပေါ်ပေါက်ခဲ့သော ADSL ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 2000000bit/s (2Mbit/s)  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->   ADSL 
+data    --->   speed = 2000000bit/s (2Mbit/s) 
+method  --->   download()
+
+5. 2006 တွင် ပေါ်ပေါက်ခဲ့သော Ethernet ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10Mbit/s  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->   Ethernet
+data    --->   speed = 10Mbit/s
+method  --->   download()
+
+6. 2014 တွင်  Ethernet_2014 ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->   Ethernet_2014 
+data    --->   speed = 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်)
+method  --->   download()
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+class   --->  Car 
+data    --->  VIN, tires, engine 
+method  --->
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+class   --->   Tires
+data    --->   size, pressure = 0 ( psi )
+method  --->   pump(p)
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+###################################################################################################
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+
+class   --->  Tires 
+data    --->  size, pressure = 0 
+method  --->  pump(p)
+
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+###################################################################################################
+
+Step.3   --->   Draw
+
+Create
+1. Draw design -> programmer
+2. Python      -> design, design()
+3. Computer
+4. RAM    
+
+#################################################
+
+class   --->   Laptop
+data    --->   serial_no
+method  --->   on(), off()
+
+
+class Laptop:
+    def __init__(x, y):
+        x.serial_no = y
+
+    def on(x):
+        pass
+
+    def off(x):
+        pass
+
+
+#################################################
+
+external data or prefix data
+
+speed 
+speed = 9600
+
+pump(p)
+download()
+
+#################################################
+
+speed Vs speed = 9600bit/s 
+
+class Network_Card:
+    def __init__(x, y):
+        x.speed = y
+        
+
+class DialUp:
+    def __init__(x):
+        x.speed = "9600 bit/s"
+        
+#################################################
+
+x = 20
+age = 20
+weight = 20
+
+self = memory address of self
+other = memory address of other  
+
+#################################################
+
+
+class Network_Card:
+    def __init__(self, y):
+        self.speed = y
+
+    def download(self):
+        pass
+
+
+class DialUp:
+    def __init__(self):
+        self.speed = "9600 bit/s"
+
+    def download(self):
+        pass
+
+
+n1 = Network_Card("1 MB/s") # Mandalay
+n2 = Network_Card("2 MB/s") # Yangon
+n3 = DialUp()               # Meiktila
+
+print(n1.speed)
+print(n2.speed)
+print(n3.speed)
+
+#################################################
+
+class   --->   ADSL 
+data    --->   speed = 2000000bit/s (2Mbit/s) 
+method  --->   download()
+
+
+class ADSL:
+    def __init__(self):
+        self.speed = "2000000bit/s (2Mbit/s)"
+
+    def download(self):
+        print("Download with", self.speed)
+
+
+n1 = ADSL()
+n1.download()
+
+#################################################
+
+class   --->   Ethernet
+data    --->   speed = 10Mbit/s
+method  --->   download()
+
+
+class Ethernet:
+    def __init__(self):
+        self.speed = "10Mbit/s"
+        
+    def download(self):
+        pass
+        
+
+#################################################
+
+class   --->   Ethernet_2014 
+data    --->   speed = 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်)
+method  --->   download()
+
+
+class Ethernet_2014:
+    def __init__(self):
+        self.speed = "10000Mbit/s"
+        
+    def download(self):
+        pass
+        
+        
+#################################################
+
+class   --->  Car 
+data    --->  VIN, tires, engine 
+method  --->
+
+
+class Car:
+    def __init__(self, x, y, z):
+        self.VIN = x
+        self.tires = y
+        self.engine = z
+        
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+        
+
+#################################################
+
+class   --->   Tires
+data    --->   size, pressure = 0 ( psi )
+method  --->   pump(p)
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        print(f"Pump with {p} psi.")
+        self.pressure = p
+
+
+t = Tires(18)
+t2 = Tires(15)
+
+t.pump(10)
+print(t.pressure)
+
+t2.pump(7)
+print(t2.pressure)
+
+#################################################
+
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+        
+    def on(self):
+        pass
+    
+    def off(self):
+        pass
+        
+        
+#################################################
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။                              Car     (VIN, tires, engine)
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။  
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။                       Tires       (size, pressure = 0), pump(p) 
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+#################################################
+
+class   --->  
+data    --->  
+method  --->
+
+class   --->  
+data    --->  
+method  --->
+
+#################################################
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+class   --->  Tires 
+data    --->  size, pressure = 0 
+method  --->  pump(p)
+
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+
+class   --->  Car
+data    --->  VIN, tires, engine
+method  --->
+
+#################################################
+
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        print(f"Pump with {p} psi.")
+        self.pressure = p
+        
+        
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+        
+    def on(self):
+        pass
+    
+    def off(self):
+        pass
+        
+        
+#################################################
+
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        print(f"Pump with {p} psi.")
+        self.pressure = p
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        print("Engine on.")
+        self.state = "on"
+
+    def off(self):
+        print("Engine off.")
+        self.state = "off"
+
+
+# "BMW-001A", 18, petrol
+# 8, on()
+
+off_road_car = Car(VIN="BMW-001A", tires=Tires(18), engine=Engine("petrol"))
+off_road_car.tires.pump(8)
+off_road_car.engine.on()
+
+#################################################
+
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        print(f"Pump with {p} psi.")
+        self.pressure = p
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        if self.state == "off":
+            print("Engine on.")
+            self.state = "on"
+        else:
+            print("Already on.")
+
+    def off(self):
+        if self.state == "on":
+            print("Engine off.")
+            self.state = "off"
+        else:
+            print("Already off.")
+
+
+# off_road_car -> "BMW-001A", 18, petrol
+# 8 psi, on()
+
+off_road_car = Car(VIN="BMW-001A", tires=Tires(18), engine=Engine("petrol"))
+off_road_car.tires.pump(8)
+off_road_car.engine.on()
+
+#################################################
+
+2. Divide            ->   15 sec
+3. Draw              ->   30 sec
+4. car, pump, on     ->   50 sec
+
+total                ->   1 min 35 sec
+
+5. control           ->   1 min
+   - data
+     - pressuse, state, 
+   - fun
+     - on(), off()
+     
+total                ->   ? <= 2 min 35 sec
+
+##################################################################################################
+
+"""
+
+
