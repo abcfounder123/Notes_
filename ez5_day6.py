6461127255b5345b521faed96c2b7fbebdef14bd
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-12-13 12:27:31 +0700

    day.6

---
+++ ez5_day6.py
@@ -0,0 +1,121 @@
+
+"""
+
+ordered collections
+1. str
+2. list
+3. tuple
+4. bytearray
+5. bytes
+
+unordered collections
+1. set
+2. frozenst
+3. dict
+
+################################################
+
+mutable data (update, delete)
+1. list       ordered, mutable
+2. set        unordered mutable
+3. bytearray
+5. dict
+
+immutable data (15 - 5)
+
+################################################
+
+1. Operation, Operator, Operand
+
+1 + 2    <---   Additional Operation
++        <---   Additional Operator
+1, 2     <---   Operand
+1        <---   Left Operand
+2        <---   Right Operand
+
+################################################
+
+2. Types of operator 
+   - unary operator    (right)
+   - binary operator   (l, r)
+   - ternary operator  (middle)
+
+################################################
+
+3. positive, negative, addition, subtraction
+
++1          <- pos
+-1          <- neg
+1 + 2       <- addition
+1 - 2       <- subtraction
+
+################################################
+
+1 + 2  ->  add
+1 - 2  ->  sub
+-2     ->  neg
+1 * 2  ->  multiplication, mul
+1 / 2  ->  division, div
+
+################################################
+
+Precedence
+
+add
+1 + 2 * 3
+3 * 3
+9
+
+mul
+1 + 2 * 3
+1 + 6
+7
+
+################################################
+
+Arithmetic operators (9)(e u */ +-)
+
+division        =>   5 / 2    =>  2.5
+floor division  =>   5 // 2   =>  2
+modulus         =>   5 % 2    =>  1
+
+################################################
+
+1. Procedure              4
+2. Sequence               3
+3. Data types            15
+4. Creating by names     14
+5. Creating by symbols   11     q, ., j, [], (), {}, {}, b, 1, True, None
+6. range                  3
+7. length   -> len()
+8. delete   -> del 
+9. Indexing(+, -, hard, middle)
+10. Slicing (start:stop:step, half)
+11. collections and indexing ( base + size * n ) (n = i + 1)
+12. Characteristics of data types
+    - ordered, unordered, mutable, immutable
+13. collections (8)
+    - ordered collections (5)
+    - unordered collections (3)
+14. mutable data(5), immutable data(10)
+15. Operation, Operator, Operand
+16. Types of operator (unary, binary, ternary)
+17. Precedence
+18. Arithmetic operators (9)(e u */ +-)
+
+################################################################################################
+
+Knowledge
+1. collection   -> "abcdefg", ["apple", "banana", "orange", [70, 73, 80, "Grade.4"]], [str, str, str, list]
+2. element      -> a, 
+3. absolute value -> abs of -5  -> 5
+4. range of collection, out of range error   
+5. 1/2, 1/3, 1/4
+6. Low level programming language
+7. High level programming language 
+
+################################################################################################
+
+"""
+
+
