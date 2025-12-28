66ec7216feacc87ff8ec7247d0cc9a29288cd8bd
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2025-12-28 11:56:40 +0700

    day 11

--- ez5_day10.py
+++ ez5_day10.py
@@ -210,4 +210,307 @@ print("A\n\tB\n\t\tC")
 
 ################################################################################################
 
+Day.11
+
+encode(A => \u0041) 
+decode(\u0041 => A)
+
+################################################
+
+4. single line string 
+   - single quotes, double quotes
+   - 'appple', "apple"
+   - can not encode "new line chr"      --->   'A\u000aB'
+   
+   - single quotes can not encode '     --->   'I\u0027m abc.'    --->   'I\'m abc.'   --->  "I'm abc."
+   - doublele quotes can not encode "   --->   "\u0022My Family\u0022"    --->   "\"My Family\""  --->  '"My Family"'
+ 
+39   0x27  single quote
+34   0x22  double quote
+                     
+'A
+B'
+
+unterminated str  
+                  
+################################################
+
+y = "\nAge = 20\n\tName = Mg Mg\n\t\tMarks = 100\n\t\t\tRoll No = 1"
+print(y)  
+
+################################################
+
+'''
+Age = 20
+    Name = Mg Mg
+        Marks = 100
+            Roll No = 1
+'''
+
+################################################
+
+Triple quotes can encode new lines.
+
+5. multiline string
+6. preformatted string   
+7. documentation string
+   
+################################################
+
+8. square root ( exponent 1/2 )   
+
+4 ** 2 
+4 * 4 
+16
+
+
+16 ** (1/2)
+4
+
+square root of 16 = 4
+
+################################################
+
+9. cube root ( exponent 1/3 ) 
+
+4 ** 3
+4 * 4 * 4
+64
+
+64 ** (1/3)
+4
+
+cube root of 64 = 4
+
+################################################
+
+10. Sequence
+
+Ohm’s Law (Voltage, Current, Resistance)( V = IR )
+
+V = I * R
+I = V / R
+R = V / I
+
+r = float(input("Resistance = "))
+i = float(input("Current = "))
+v = i * r
+
+print(f"Voltage = {v}")
+
+################################################
+
+print(f"Voltage = {v}")
+
+print(f"Voltage = {i * r}")
+
+print(f"Voltage = {float(input("Current = ")) * float(input("Resistance = "))}")
+
+################################################
+
+1. input("Current = ")
+2. float()
+3. input("Resistance = ") 
+4. float()
+5. mul()
+6. format() 
+7. print() 
+
+################################################
+
+1. input
+2. float
+3. assign
+4. input
+5. float
+6. assign
+7. mul
+8. assign
+9. format
+10. print
+
+################################################
+
+11. unicode
+
+65       A
+97       a
+4096     က
+
+################################################
+
+12. character to unicode number (ord(က) -> 4096)
+    - ordinal unicode number of a character (ord)
+
+13. unicode number to character (chr(4096) -> က)
+    - unicode character (chr) 
+
+################################################
+
+14. uppercase to lowercase  ( A to a )
+
+- unicode of A          65
+- unicode of A + 32     97
+- unicode number of 'a' to character
+
+"A"  ->  65  + 32  ->  97  ->  "a"
+
+u = input("Uppercase Character = ")
+l = chr(ord(u) + 32)
+print(f"Lowercase Character = {l}")
+
+l = input("Lowercase Character = ")
+u = chr(ord(l) - 32)
+print(f"Uppercase Character = {u}")
+
+################################################
+
+15. Algorithms 
+
+Algorithm for uppercase to lowercase 
+- add 32
+
+Algorithm for lowercase to uppercase
+- substract 32
+
+################################################
+
+16. pseudo code
+
+1. unicode of uppercase   
+2. unicode of uppercase + 32    
+3. unicode number of lowercase to character
+
+################################################
+
+uppercase = ord(input("uppercase = ")) # A   ->  65
+lowercase = chr(uppercase + 32)        # 97  ->  a
+print("lowercase =", lowercase)
+
+################################################
+
+Algorithm for lowercase to uppercase ( -32 )
+
+- unicode of lowercase   
+- unicode of lowercase - 32    
+- unicode number of uppercase to character
+
+lowercase = ord(input("lowercase = ")) # a   ->  97
+uppercase = chr(lowercase - 32)        # 65  ->  A
+print("uppercase =", uppercase)
+
+################################################
+
+Algorithm for A to  က ( + 4031 )
+
+65 to 4096  
+
+################################################################################################
+
+CLI (35)
+
+1. Water Intake Calculator
+
+weight = float(input("Enter your weight in kg: "))
+min = weight * 0.033
+max = weight * 0.050
+print(f"You should drink from {min:.2f} liters to {max:.2f} litres.")
+
+1 kg = 2.2 lb       ( lb = kg * 2.2 )    ( kg = lb / 2.2 )
+1 lb = 0.4535 kg    ( kg = lb x 0.4535 ) ( lb = kg / 0.4535 ) 
+
+1$ = 5000 kyats
+
+* 5000
+10$ = 50000 kyats 
+
+/ 5000
+50000 kyats = 10$
+
+
+1 kyats = 0.0002$
+
+* 0.0002
+50000 kyats = 10$
+
+/ 0.0002
+10$ = 50000 kyats
+
+################################################
+
+2. Area of the circle ( pi.r**2 )
+
+r = float(input("Enter radius: "))
+area = 3.14159 * r ** 2
+print("Area of the circle:", area)               
+
+################################################
+
+3.Area of triangle ( 1/2.b.h) (05.b.h)
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+area = 0.5 * base * height
+print("Area of triangle:", area)
+
+################################################
+
+4. Hypotenuse ( c**2 = a**2 + b**2 )  ( c**(2/1) = a**2 + b**2 ) ( c = (a**2 + b**2) ** (1/2) )
+
+
+|\\
+| \\ c
+|b \\
+|...\\
+  a  
+                 
+c2 = 16
+c = 16 ** (1/2) = 4
+  
+  
+16 ** 1/2
+16 / 2
+8
+
+16 ** (1/2)
+16 ** 0.5
+4.0
+
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+h = (a ** 2 + b ** 2) ** (1/2)
+print("Hypotenuse:", h)
+
+################################################
+
+5. Ohm’s Law (Voltage, Current, Resistance)( V = IR ) ( I = V/R ) ( R = V/I )
+
+voltage = float(input("Enter voltage (V): "))     
+resistance = float(input("Enter resistance (Ω): "))
+current = voltage / resistance
+print("Current:", current, "A")
+
+################################################
+
+6. Speed ( s = d / t )
+
+3m + 3m + 3m + .. = 30m
+
+speed = 3m/s
+time = 10 seconds
+distance = ? = 3m/s * 10s = 30m
+d = s * t
+
+10 sec, 30m, 
+s = ?
+1 sec   ?
+30 / 10, d/t 
+
+distance = float(input("Enter distance (m): "))
+time = float(input("Enter time (s): "))
+speed = distance / time
+print("Speed:", speed, "m/s")
+
+################################################
+
 """
