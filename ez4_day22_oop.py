23ea9b50680e5f7c0846a2703cda0aa265bdb55b
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-01-27 00:23:41 +0700

    day23, step.3, 4, 5

--- ez4_day22_oop.py
+++ ez4_day22_oop.py
@@ -196,4 +196,409 @@ method  --->  on(), off()
 
 ###################################################################################################
 
+Step.3   --->   Draw design
+
+1. Design - 15
+2. select design -> int
+3. create obj    -> ()
+4. obj is in RAM
+5. label holds "address of object"
+6. design paper, label card , house object
+7. checking memory address(ID No) -> id()
+8. directory for object -> dir
+9. initial method, constructor method -> init
+10. prefix data or external data
+11. function with obj data
+12. function with obj data + external data
+13. self and other
+14. lower_snake_case and UpperCamealCase
+15. Creating an object
+
+#################################################
+
+6. design paper, label card , house object
+
+x = int()
+
+label        => x (hold address)(4396535304)
+design paper => int
+creat        => ()
+object       => RAM
+
+#################################################
+
+10. prefix data or external data 
+
+
+class Tires:
+    def __init__(self):
+        self.pressure = 0
+        
+
+class Laptop:
+    def __init__(self, x):
+        self.serial_no = x
+        
+
+#################################################
+
+11. function with obj data
+
+
+def download(self):
+    print("Download with", self.speed)
+
+
+#################################################
+
+12. function with obj data + external data
+
+
+def pump(self, p):
+    print(f"pump to {p} psi.")
+
+
+#################################################
+
+13. self and other
+
+self = memory address of self
+other = memory address of other  
+
+#################################################
+
+14. lower_snake_case and UpperCamealCase
+
+electric car
+electric_car    lower_snake_case (data, fun, method, ... )
+ElectricCar     UpperCamealCase  (class)
+
+#################################################
+
+15. Creating an object
+
+1. Draw design -> programmer
+2. Python      
+3. Computer
+4. RAM  
+
+##################################################################################################
+
+1. Laptop တွင် serial_no ပါသည်။ on, off လုပ်နိုင်သည်။
+
+class   --->   Laptop
+data attributes   --->   serial_no
+method attributes --->   on(), off()
+
+class Laptop:
+    def __init__(self, serial_no):
+        self.serial_no = serial_no
+
+    def on(self):
+        pass
+
+    def off(self):
+        pass
+        
+#################################################
+
+2. Network_Card တွင် speed ပါသည်။ သတ်မှတ် speed ဖြင့် download ဆွဲနိုင်သည်။
+
+class   --->  Network_Card 
+data    --->  speed 
+method  --->  download() 
+
+class Network_Card:
+    def __init__(self, y):
+        self.speed = y
+
+    def download(self):
+        pass
+
+#################################################
+
+3. 1995 တွင် ပေါ်ပေါက်ခဲ့သော DialUp ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 9600bit/s ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+class   ---> DialUp  
+data    ---> speed = 9600bit/s 
+method  ---> download()
+
+class DialUp:
+    def __init__(self):
+        self.speed = "9600 bit/s"
+
+    def download(self):
+        pass
+
+################################################# 
+
+4. 1999 တွင် ပေါ်ပေါက်ခဲ့သော ADSL ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 2000000bit/s (2Mbit/s)  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+class   --->  ADSL 
+data    --->  speed = 2000000bit/s (2Mbit/s) 
+method  --->  download()
+
+class ADSL:
+    def __init__(self):
+        self.speed = "2000000bit/s (2Mbit/s)"
+
+    def download(self):
+        print("Download with", self.speed)
+
+#################################################
+
+5. 2006 တွင် ပေါ်ပေါက်ခဲ့သော Ethernet ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10Mbit/s  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+class   --->  Ethernet 
+data    --->  speed = 10Mbit/s 
+method  --->  download()
+
+class Ethernet:
+    def __init__(self):
+        self.speed = "10Mbit/s"
+
+    def download(self):
+        pass
+
+#################################################
+
+6. 2014 တွင်  Ethernet_2014 ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+class   --->  Ethernet_2014 
+data    --->  speed = 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) 
+method  --->  download()
+
+class Ethernet_2014:
+    def __init__(self):
+        self.speed = "10000Mbit/s"
+
+    def download(self):
+        pass
+
+#################################################
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+
+class   --->  Car 
+data    --->  VIN, tires, engine 
+method  ---> 
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+        
+#################################################
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+
+class   --->  Tires 
+data    --->  size, pressure = 0
+method  --->  pump(p)
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        self.pressure = p
+        print(f"pump to {p} psi.")
+
+#################################################
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+class   --->  Engine  
+data    --->  fuel_type, state = "off"
+method  --->  on(), off()
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        print(f"{self.fuel_type} Engine On.")
+
+    def off(self):
+        print(f"{self.fuel_type} Engine Off.")
+
+#################################################
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။                              Car     (VIN, tires, engine)
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။  
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။                       Tires       (size, pressure = 0), pump(p) 
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+#################################################
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+class   --->  Tires 
+data    --->  size, pressure = 0 
+method  --->  pump(p)
+
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+
+class   --->  Car
+data    --->  VIN, tires, engine
+method  --->
+
+#################################################
+
+
+class Car:
+    def __init__(self, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+        
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+    
+    def pump(self, p):
+        print(f"pump to {p} psi.")
+        
+        
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        print(f"{self.fuel_type} Engine On.")
+
+    def off(self):
+        print(f"{self.fuel_type} Engine Off.")
+
+
+# city_car -> "BMW-0001", 15" tires, Petrol engine
+# 7psi, on
+
+city_car = Car(VIN="BMW-0001", tires=Tires(15), engine=Engine("Petrol"))
+city_car.tires.pump(7)
+print(city_car.engine.state)
+
+##################################################################################################
+
+Step.4   --->   Controlling data by function  ( pressure, state )
+
+
+class Car:
+    def __init__(self, *, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        self.pressure = p
+        print(f"pump to {p} psi.")
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        self.state = "on"
+        print(f"{self.fuel_type} Engine On.")
+
+    def off(self):
+        self.state = "off"
+        print(f"{self.fuel_type} Engine Off.")
+
+
+
+city_car = Car(VIN="BMW-0001", tires=Tires(15), engine=Engine("Petrol"))
+
+print(city_car.engine.state)
+city_car.engine.on()
+print(city_car.engine.state)
+
+#################################################
+
+Step.5   --->   Controlling function by data  ( on(), off() )
+
+
+class Car:
+    def __init__(self, *, VIN, tires, engine):
+        self.VIN = VIN
+        self.tires = tires
+        self.engine = engine
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        self.pressure = p
+        print(f"pump to {p} psi.")
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        if self.state == "off":
+            self.state = "on"
+            print(f"{self.fuel_type} Engine On.")
+        else:
+            print("Already On")
+
+    def off(self):
+        if self.state == "on":
+            self.state = "off"
+            print(f"{self.fuel_type} Engine Off.")
+        else:
+            print("Already Off")
+
+
+city_car = Car(VIN="BMW-0001", tires=Tires(15), engine=Engine("Petrol"))
+
+city_car.engine.on()
+city_car.engine.on()
+city_car.engine.on()
+city_car.engine.on()
+
+city_car.engine.off()
+city_car.engine.off()
+city_car.engine.off()
+
+##################################################################################################
+
+
 """
