e98c1d18b8624f4ef9b2e4dad28488054953d3bc
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-02-08 12:58:48 +0700

    day.20 and 21, exercises 22

--- ez5_day20_exercises.py
+++ ez5_day20_exercises.py
@@ -1,5 +1,8 @@
 
 """
+
+Day.20
+
 1. is_even (တန်ဖိုးတစ်ခုခုကို 2 နဲ့စားလို့ အကြွင်း 0 ရရင် စုံကိန်းဖြစ်ပါတယ်။)( n % 2 == 0)
 
 2. is_odd (တန်ဖိုးတစ်ခုခုကို 2 နဲ့စားလို့ အကြွင်း 1 ရရင် မကိန်းဖြစ်ပါတယ်။) ( n % 2 == 1 )
@@ -204,6 +207,123 @@ def count_vowels(s):
             else:
                 d[c] += 1
     return d
+
+################################################
+
+16. sum_of_list(lst) (စာရင်းထဲက နံပါတ်တွေကို ပေါင်းခြင်း။)
+
+
+def sum_of_list(lst):
+    t = 0
+    for n in lst:
+        t += n
+    return t
+
+
+################################################
+
+17. max(lst) (အများဆုံးတန်ဖိုး ရှာခြင်း။)
+
+
+def max(lst):
+    m = lst[0]  
+    for n in lst[1:]:
+        m = greater_number(m, n) 
+    return m
+
+
+################################################    
+
+18. min(lst) (အနည်းဆုံးတန်ဖိုး ရှာခြင်း။)
+
+
+def min(lst):
+    m = lst[0] 
+    for n in lst[1:]:
+        m = less_number(m, n)
+    return m
+
+
+################################################
+
+19. find_max_min(lst) အများဆုံးနဲ့ အနည်းဆုံးတန်ဖိုး ရှာခြင်း။ 
+
+
+def find_max_min(lst):
+    return max(lst), min(lst)
+    
+    
+################################################
+
+21. Uppercase and Lowercase
+
+Uppercase (a to A)  (97 to 65)
+
+
+def upper(s):
+    ans = ""
+    for c in s:   # "g"
+        if c in "abcdefghijklmnopqrstuvwxyz":
+            ans += chr(ord(c) - 32)
+        else:
+            ans += c
+    return ans
+    
+    
+################################################ 
+  
+Lowercase (A  to a)  (65 to 97)
+
+
+def lower(s):
+    ans = ""
+    for c in s:  
+        if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            ans += chr(ord(c) + 32)
+        else:
+            ans += c
+    return ans
+    
+
+################################################ 
+
+20. linear search
+
+
+def linear_search(lst, element):
+    for e in lst:
+        if e == element:
+            return True
+    return False
+
+
+################################################ 
+
+22. Binary Search
+
+
+def binary_search(collection, element):
+    while len(collection) > 0:
+        t = len(collection)               
+        m = t // 2                        
+        middle = collection[m]           
+
+        if element == middle:
+            return True
+        elif element > middle:
+            collection = collection[m+1:]     # r
+        elif element < middle:
+            collection = collection[:m]       # l     
+    return False
+    
+ 
+################################################
+
+10. leap year (ရက်ထပ်နှစ်) (and, or)
+
+
+def leap_year(y):
+    return ( y % 400 == 0 ) or ( y % 4 == 0 and y % 100 != 0 )
 
 
 ################################################################################################
@@ -730,6 +850,300 @@ print(count_vowels(x))
 
 ################################################
 
+16. sum_of_list(lst) (စာရင်းထဲက နံပါတ်တွေကို ပေါင်းခြင်း။)
+
+
+def sum_of_list(lst):
+    t = 0
+    for n in lst:
+        t += n
+    return t
+
+
+x = [100, 1000, 2000]
+print(sum_of_list(x))
+
+################################################
+
+17. max(lst) (အများဆုံးတန်ဖိုး ရှာခြင်း။)
+
+x = [100, 1000, 2000, 1500, 3000]
+
+m = 100
+
+100  1000    m = 1000
+1000 2000    m = 2000
+2000 1500    m = 2000
+2000 3000    m = 3000
+
+m = 3000
+
+################################################
+
+
+def greater_number(n1, n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+
+
+def max(lst):
+    m = lst[0]  
+    for n in lst[1:]:
+        m = greater_number(m, n) 
+    return m
+
+
+x = [100, 1000, 2000, 1500, 3000]
+print(max(x))
+        
+
+################################################    
+
+18. min(lst) (အနည်းဆုံးတန်ဖိုး ရှာခြင်း။)
+
+
+def min(lst):
+    m = lst[0] 
+    for n in lst[1:]:
+        m = less_number(m, n)
+    return m
+
+
+################################################
+
+
+19. find_max_min(lst) အများဆုံးနဲ့ အနည်းဆုံးတန်ဖိုး ရှာခြင်း။ 
+
+(3000, 100)
+
+
+def find_max_min(lst):
+    return (max(lst), min(lst))
+
+
+def find_max_min(lst):
+    return max(lst), min(lst)
+    
+    
+################################################
+
+[3000, 100]
+
+
+def find_max_min(lst):
+    return [max(lst), min(lst)]
+    
+    
+################################################
+
+{'max': 3000, 'min': 100}
+
+
+def find_max_min(lst):
+    return {"max": max(lst), "min": min(lst)}
+
+
+################################################
+
+
+def greater_number(n1, n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+
+
+def less_number(n1, n2):
+    if n1 < n2:
+        return n1
+    else:
+        return n2
+
+
+def max(lst):
+    m = lst[0]  # 100
+    for n in lst[1:]: # [1000, 2000, 1500, 3000]
+        m = greater_number(m, n)
+    return m
+
+
+def min(lst):
+    m = lst[0]  # 100
+    for n in lst[1:]: # [1000, 2000, 1500, 3000]
+        m = less_number(m, n)
+    return m
+
+
+def find_max_min(lst):
+    return max(lst), min(lst)
+
+
+x = [100, 1000, 2000, 1500, 3000]
+ans = find_max_min(x)
+print(ans)
+
+################################################
+
+
+def find_max_min(lst):
+    max = lst[0]  
+    for n in lst[1:]:  
+        if max > n:
+            pass
+        else:
+            max = n
+
+    min = lst[0]  
+    for n in lst[1:]: 
+        if min < n:
+            pass
+        else:
+            min = n
+    return max, min
+
+
+################################################
+
+21. Uppercase and Lowercase
+
+Uppercase (a to A)  (97 to 65)
+
+1. ord
+2. - 32
+3. chr
+
+chr(ord(c) - 32)
+
+################################################ 
+
+
+def upper(s):
+    ans = ""
+    for c in s:   # "g"
+        if c in "abcdefghijklmnopqrstuvwxyz":
+            ans += chr(ord(c) - 32)
+        else:
+            ans += c
+    return ans
+    
+    
+################################################ 
+  
+Lowercase (A  to a)  (65 to 97)
+
+1. ord
+2. + 32
+3. chr
+
+chr(ord(c) + 32)
+
+################################################ 
+
+
+def lower(s):
+    ans = ""
+    for c in s:  
+        if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            ans += chr(ord(c) + 32)
+        else:
+            ans += c
+    return ans
+
+
+x = "I go to school."
+print(lower(x))
+
+################################################ 
+
+20. linear search
+
+
+def linear_search(lst, element):
+    for e in lst:
+        if e == element:
+            return True
+    return False
+
+
+################################################ 
+
+atoms = ['Aluminium', 'Antimony', 'Argon', 'Arsenic', 'Astatine', 'Barium', 'Barium', 'Beryllium', 'Boron', 'Bromine', 'Cadmium', 'Calcium', 'Carbon', 'Cerium', 'Cesium', 'Chlorine', 'Chromium', 'Cobalt', 'Copper', 'Dysprosium', 'Erbium', 'Europium', 'Fluorine', 'Francium', 'Gadolinium', 'Gallium', 'Germanium', 'Gold', 'Hafnium', 'Helium', 'Holmium', 'Hydrogen', 'Indium', 'Iodine', 'Iron', 'Krypton', 'Lanthanum', 'Lithium', 'Lutetium', 'Magnesium', 'Manganese', 'ManganeseGermanium', 'Mercury', 'Molybdenum', 'Neodymium', 'Neon', 'Nickel', 'Niobium', 'Nitrogen', 'Oxygen', 'Palladium', 'Phosphorus', 'Platinum', 'Potassium', 'Praseodymium', 'Promethium', 'Radium', 'Radon', 'Rhodium', 'Rubidium', 'Ruthenium', 'Samarium', 'Scandium', 'Selenium', 'Silicon', 'Silver', 'Sodium', 'Strontium', 'Strontium', 'Sulfur', 'Tantalum', 'Technetium', 'Tellurium', 'Terbium', 'Thulium', 'Tin', 'Titanium', 'Tungsten', 'Vanadium', 'Xenon', 'Ytterbium', 'Yttrium', 'Zinc', 'Zirconium']
+
+################################################ 
+
+22. Binary Search
+
+Search
+1. e == m
+2. e > m   =>  right
+3. e < m   =>  left
+
+################################################ 
+
+empty => Stop 
+
+any value => Search
+
+################################################ 
+
+x = [10, 40, 50, 70, 90, 110, 120]
+
+total = len(x)          =>  7
+m = t // 2              =>  3
+middle = x[m]           =>  70
+
+right = [m+1:end]       =>  [90, 110, 120]
+left = [start:m]        =>  [10, 40, 50]
+
+################################################   
+
+
+def binary_search(collection, element):
+
+    while len(collection) > 0:
+        t = len(collection)                # 7        3        1
+        m = t // 2                         # 3        1        0
+        middle = collection[m]             # 70       40       10
+
+        if element == middle:
+            return True
+        elif element > middle:
+            collection = collection[m+1:]     # r
+        elif element < middle:
+            collection = collection[:m]       # l     [10, 40, 50]   [10]    []
+
+    return False
+    
+ 
+################################################
+
+10. leap year (ရက်ထပ်နှစ်) (and, or)
+    1. divisible by 400 ( eg. 2000, 1600 )    ( y % 400 == 0 )
+    2. divisible by 4 + not divisible by 100   ( y % 4 == 0 and y % 100 != 0 )
+    
+    - Rule.1 or Rule.2
+
+################################################
+
+1. Julian
+
+
+def leap_year(y):
+    return y % 4 == 0
+    
+    
+################################################
+
+2. Gregorian
+
+
+def leap_year(y):
+    return ( y % 400 == 0 ) or ( y % 4 == 0 and y % 100 != 0 )
+    
+
+################################################################################################
 
 """
 
