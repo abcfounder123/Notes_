816f687a0d78cbdefdc520c6637a64b3e8e0ecf3
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-03-31 23:01:53 +0700

    t

---
+++ ez7_type_casting.py
@@ -0,0 +1,47 @@
+
+"""
+Type casting (21) (14 + c o 2 8 16 r e)
+
+1. str()
+
+2. int()
+3. float()
+4. complex()
+
+5. list()
+6. tuple()
+7. set()
+8. frozenst()
+9. bytearray()
+10. bytes()
+11. range()
+12. dict()
+
+13. bool()
+
+14. memoryview()
+
+15. chr 
+16. ord
+17. bin 
+18. oct
+19. hex 
+20. repr 
+21. eval 
+
+################################################
+
+c o 2 8 16 r e
+
+
+1. chr()                                65  to   "A"
+2. ordinal number, ord()                "A" to   65          int
+3. binary code, bin()                   2   to   "0b10"
+4. octal code, oct()                    8   to   "0o10"
+5. hexadecimal code, hex()              16  to   "0x10"      
+6. representation string, repr()        1   to   "1"        
+7. evaluation, eval()                   str to program       , 15
+
+################################################
+
+"""
